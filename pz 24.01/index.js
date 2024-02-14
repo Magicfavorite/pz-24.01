0d491c21231f32ab23c6f5eb9c5cc30de1b70a4c
Magicfavorite <131023322+Magicfavorite@users.noreply.github.com>
2024-02-14 20:14:08 +0300

    Add files via upload

---
+++ pz 24.01/index.js
@@ -0,0 +1,59 @@
+// this function calculates mean value
+/*******************
+ Computation
+*******************/
+
+/*******************
+ Displaying
+*******************/
+
+
+a = b % 2 // % — remainder of division
+semesterBegin = '2018-06-04' // from itstep.org site
+
+(login=="user" || email=="user@itstep.org") &&
+password=="MySecret"
+age>=30 && age<=40
+///
+(age>=30 && age<=40) && (education=="higher") && !
+(profession == "banker")
+///
+(age>=30 && age<=40) && (education=="higher") && !
+(profession == "banker")
+////Условия
+age>=30 && age<=40 && education==
+"higher" && !profession == "banker" 
+
+if(condition) {
+    statement_if_true1;
+    statement_if_true2;
+    statement_if_true3;
+    }
+    else {
+    statement_if_false1;
+    statement_if_false2;
+    statement_if_false3;
+    }
+
+    if( Math.round(x) % 2 == 0 )
+    bx = Math.round(x)
+   else {
+    if( x < Math.round(x) )
+  
+    bx = Math.round(x) + 1
+   }
+
+///«switch».
+switch(protocol){
+    case "HTTP":
+    description = "Hypertext transfer protocol";
+    break;
+    case "HTTPS":
+  
+    break;
+    case "FTP":
+    description = "File transfer protocol";
+    break;
+    default :
+    description = "Unsupported protocol";
+   }
